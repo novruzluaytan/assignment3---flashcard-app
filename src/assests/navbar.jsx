575f71aea0966377fcfb66c029dffee3a9c9f132
Aytan Novruzlu <aytannovruzlu@Aytans-MacBook-Air-4.local>
2024-01-02 01:16:56 +0400

    added links to the navbar.jsx file and orginezed the files

--- src/assests/navbar.jsx
+++ src/assests/navbar.jsx
@@ -3,18 +3,13 @@ import React from 'react';
 
 function navbar() {
   return (
-    <div className="container">
-      <div className="navbar">
-        <div className="homePage">
-          <Link to="/">Home Page</Link>
-        </div>
-        <div className="flashcard-app">
-          <Link to="/flashcard">Flashcard app</Link>
-        </div>
-        <div className="contact">
-          <Link to="/contact">Contact Me</Link>
-        </div>
+    <div className="navbar">
+      <div className='pages'>
+        <NavLink to='/home'>Home</NavLink>
+        <NavLink to='/cards'>Flashcard app</NavLink>
+        <NavLink to='/contact'>Contact Me</NavLink>
       </div>
+
     </div>
 
   );
